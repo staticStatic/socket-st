0e74f6f42c0ceabdc8a0eb26fa0027067489766a
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-08-04 21:55:03 -0500

    new room for x and y uyu

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,5 +1,5 @@
 import { Room, Client } from "colyseus";
-import { MyRoomState } from "./schema/MyRoomState";
+import { MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
 
@@ -14,15 +14,21 @@ export class MyRoom extends Room<MyRoomState> {
 
     this.onMessage("message", (client, message) => {
       this.broadcast('messages', `(${client.sessionId}) ${message}`);
-      //client.send(message)
-      //console.log("woohoo", message)
     })
 
+    this.onMessage("move", (client, data) => {
+      console.log(data)
+      console.log("StateHandlerRoom received message from", client.sessionId, ":", data);
+      this.state.movePlayer(client.sessionId, data);
+  });
   }
+
+  
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
     this.broadcast("messages", `${ client.sessionId } joined.`);
+    this.state.players.set(client.sessionId, new Player());
   }
 
   onLeave (client: Client, consented: boolean) {
--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,35 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { Schema, Context, MapSchema, type } from "@colyseus/schema";
+
+/*export class MyRoomState extends Schema {
+  @type("string") mySynchronizedProperty: string = "Hello world";
+}*/
+
+export class Player extends Schema {
+  @type("number") 
+  x: number = 0;
+  @type("number") 
+  y: number = 0;
+}
 
 export class MyRoomState extends Schema {
+  @type({ map: Player }) 
+  players = new MapSchema<Player>();
 
-  @type("string") mySynchronizedProperty: string = "Hello world";
+  createPlayer(sessionId: string) {
+    this.players.set(sessionId, new Player());
+  }
 
+  removePlayer(sessionId: string) {
+      this.players.delete(sessionId);
+  }
+
+  movePlayer (sessionId: string, movement: any) {
+    if (movement.x) {
+        this.players.get(sessionId).x += movement.x * 10;
+
+    } else if (movement.y) {
+        this.players.get(sessionId).y += movement.y * 10;
+    }
+    
+}
 }
